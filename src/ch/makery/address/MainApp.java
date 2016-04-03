36769de597cd0c6c67e6aa7462be06a928711b9c
Carlos Luiz <carloslouterio15@gmail.com>
2016-04-03 13:33:49 -0300

    PersonOverview

--- src/ch/makery/address/MainApp.java
+++ src/ch/makery/address/MainApp.java
@@ -1,53 +1,53 @@
 package ch.makery.address;
 
 import java.io.IOException;
-// ... APÓS AS OUTRAS VARIÁVEIS ...
-
-/**
- * Os dados como uma observable list de Persons.
- */
-private ObservableList<Person> personData = FXCollections.observableArrayList();
-
-/**
- * Construtor
- */
-public MainApp() {
-    // Add some sample data
-    personData.add(new Person("Hans", "Muster"));
-    personData.add(new Person("Ruth", "Mueller"));
-    personData.add(new Person("Heinz", "Kurz"));
-    personData.add(new Person("Cornelia", "Meier"));
-    personData.add(new Person("Werner", "Meyer"));
-    personData.add(new Person("Lydia", "Kunz"));
-    personData.add(new Person("Anna", "Best"));
-    personData.add(new Person("Stefan", "Meier"));
-    personData.add(new Person("Martin", "Mueller"));
-}
-
-/**
- * Retorna os dados como uma observable list de Persons. 
- * @return
- */
-public ObservableList<Person> getPersonData() {
-    return personData;
-}
-
-// ... O RESTANTE DA CLASSE ...
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import ch.makery.address.model.Person;
+import ch.makery.address.view.PersonOverviewController;
 
 public class MainApp extends Application {
-	
 
     private Stage primaryStage;
     private BorderPane rootLayout;
+    
+    /**
+     * The data as an observable list of Persons.
+     */
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
 
-    @Overridea
+    /**
+     * Constructor
+     */
+    public MainApp() {
+        // Add some sample data
+        personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
+    }
+
+    /**
+     * Returns the data as an observable list of Persons. 
+     * @return
+     */
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
+
+    @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
@@ -88,6 +88,11 @@ public class MainApp extends Application {
             
             // Set person overview into the center of root layout.
             rootLayout.setCenter(personOverview);
+            
+            // Give the controller access to the main app.
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -104,5 +109,4 @@ public class MainApp extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
