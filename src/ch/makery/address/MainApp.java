a8cf13c6739631793c24593b54d78a880d8d6731
Carlos Luiz <carloslouterio15@gmail.com>
2016-04-03 13:32:10 -0300

    add codigo

--- src/ch/makery/address/MainApp.java
+++ src/ch/makery/address/MainApp.java
@@ -1,6 +1,38 @@
 package ch.makery.address;
 
 import java.io.IOException;
+// ... APÓS AS OUTRAS VARIÁVEIS ...
+
+/**
+ * Os dados como uma observable list de Persons.
+ */
+private ObservableList<Person> personData = FXCollections.observableArrayList();
+
+/**
+ * Construtor
+ */
+public MainApp() {
+    // Add some sample data
+    personData.add(new Person("Hans", "Muster"));
+    personData.add(new Person("Ruth", "Mueller"));
+    personData.add(new Person("Heinz", "Kurz"));
+    personData.add(new Person("Cornelia", "Meier"));
+    personData.add(new Person("Werner", "Meyer"));
+    personData.add(new Person("Lydia", "Kunz"));
+    personData.add(new Person("Anna", "Best"));
+    personData.add(new Person("Stefan", "Meier"));
+    personData.add(new Person("Martin", "Mueller"));
+}
+
+/**
+ * Retorna os dados como uma observable list de Persons. 
+ * @return
+ */
+public ObservableList<Person> getPersonData() {
+    return personData;
+}
+
+// ... O RESTANTE DA CLASSE ...
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,11 +42,12 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
+	
 
     private Stage primaryStage;
     private BorderPane rootLayout;
 
-    @Override
+    @Overridea
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
@@ -71,4 +104,5 @@ public class MainApp extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+    
 }
